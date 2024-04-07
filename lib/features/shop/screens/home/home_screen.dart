619a908d51298027802116070fa4691f0ca8b02e
muhammadnaqeeb <muhammadnaqeebawan@gmail.com>
2024-04-07 12:53:42 +0500

    desiged custom shapes

--- lib/features/shop/screens/home/home_screen.dart
+++ lib/features/shop/screens/home/home_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:shopease/common/widgets/custom_shapes/containers/primary_header_container.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          TPrimaryHeaderContainer(
+            child: Column(
+              children: [],
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
