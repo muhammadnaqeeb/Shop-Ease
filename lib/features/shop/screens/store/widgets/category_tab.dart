dc619447e9a2ca16c5fd39cf14dec38bacefec8f
muhammadnaqeeb <muhammadnaqeebawan@gmail.com>
2024-04-09 07:31:39 +0500

    adding you might like section

--- lib/features/shop/screens/store/widgets/category_tab.dart
+++ lib/features/shop/screens/store/widgets/category_tab.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopease/common/widgets/brands/brand_show_case.dart';
+import 'package:shopease/common/widgets/layout/t_grid_layout.dart';
+import 'package:shopease/common/widgets/products/product_card/product_card_verticle.dart';
+import 'package:shopease/common/widgets/texts/section_heading.dart';
 import 'package:shopease/utils/constants/image_strings.dart';
 import 'package:shopease/utils/constants/sizes.dart';
 
@@ -10,20 +13,37 @@ class CategoryTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(TSizes.defaultSpace).copyWith(top: 10),
-      child: const Column(
-        children: [
-          // brand
-          TBrandShowcase(
-            images: [
-              TImages.productImage1,
-              TImages.productImage2,
-              TImages.productImage3,
+    return ListView(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace).copyWith(top: 10),
+          child: Column(
+            children: [
+              // brand
+              const TBrandShowcase(
+                images: [
+                  TImages.productImage1,
+                  TImages.productImage2,
+                  TImages.productImage3,
+                ],
+              ),
+
+              //---products
+              const SizedBox(height: TSizes.spaceBtwItems),
+              TSectionHeading(
+                title: "You might like",
+                onPressed: () {},
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              TMainGridLayout(
+                itemBuilder: (_, index) => const TProductCardVertical(),
+              )
             ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
